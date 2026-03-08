31a89e16163cc6b55063dce1342012f9248eb906
Luke Harvey Umpad <lukeumpad9597@gmail.com>
2026-03-08 20:14:38 +0800

    feat: add JSON parsing middleware and define routes for home and submit

--- index.js
+++ index.js
@@ -5,9 +5,20 @@ const port = 3000;
 // Serve static file from the "public" folder 
 app.use(express.static('public'));
 
+app.use(express.json()); // Middleware to pase JSON bodies
+
 // Define a route for the home page
+app.get('/', (req, res) => {
+    res.send('Hello, World!');
+});
+
 app.get('/about', (req, res) => {
     res.send('About us');
+});
+
+app.post('/submit', (req, res) => {
+    const data= req.body;
+    res.send(`Received: ${JSON.stringify(data)}`);
 });
 
 // Start the server
