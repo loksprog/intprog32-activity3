11f900aaec6d80eac3adf985227c82f4a80de7cd
Luke Harvey Umpad <lukeumpad9597@gmail.com>
2026-03-08 20:25:48 +0800

    style: format code for consistency and improve readability

--- index.js
+++ index.js
@@ -1,27 +1,32 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = 3000;
 
-// Serve static file from the "public" folder 
-app.use(express.static('public'));
+app.use((req, res, next) => {
+  console.log(`${req.method} ${req.url}`);
+  next();
+});
+
+// Serve static file from the "public" folder
+app.use(express.static("public"));
 
 app.use(express.json()); // Middleware to pase JSON bodies
 
 // Define a route for the home page
-app.get('/', (req, res) => {
-    res.send('Hello, World!');
+app.get("/", (req, res) => {
+  res.send("Hello, World!");
 });
 
-app.get('/about', (req, res) => {
-    res.send('About us');
+app.get("/about", (req, res) => {
+  res.send("About us");
 });
 
-app.post('/submit', (req, res) => {
-    const data= req.body;
-    res.send(`Received: ${JSON.stringify(data)}`);
+app.post("/submit", (req, res) => {
+  const data = req.body;
+  res.send(`Received: ${JSON.stringify(data)}`);
 });
 
 // Start the server
 app.listen(port, () => {
-    console.log(`Server running at http://localhost:${port}`);
+  console.log(`Server running at http://localhost:${port}`);
 });
